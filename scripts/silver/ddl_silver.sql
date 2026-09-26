e635ce860414cc5e2e159b364e40f2cd7c5f0952
Vijay Kumar <129383946+VaddanoluVijayKumar@users.noreply.github.com>
2026-09-26 11:05:13 +0530

    Alter cst_id column type in crm_cust_info table
    Changed cst_id column type from INT to NVARCHAR(50).

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -3,7 +3,7 @@
 IF OBJECT_ID ('silver.crm_cust_info' , 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info (
-    cst_id              INT,
+    cst_id              NVARCHAR(50),
     cst_key             NVARCHAR(50),
     cst_firstname       NVARCHAR(50),
     cst_lastname        NVARCHAR(50),
